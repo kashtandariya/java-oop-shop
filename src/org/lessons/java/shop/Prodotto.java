a3fb81fbb1d8a8dd5a849b98f4362ac1b2ac5af5
Dariya Kashtan <kashtandariya@gmail.com>
2024-05-27 19:17:12 +0200

    prezzo base e prezzo iva

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,18 +1,18 @@
 /*V creare la classe Prodotto che gestisce i prodotti dello shop.
- *V Un prodotto è caratterizzato da: - codice (numero intero) - nome - descrizione - prezzo - iva
-Usate opportunamente i livelli di accesso (public, private), i costruttori,
-i metodi getter e setter ed eventuali altri metodi di “utilità” per fare in modo che:
-- alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
-- il codice prodotto sia accessibile solo in lettura
-- gli altri attributi siano accessibili sia in lettura che in scrittura
-- il prodotto esponga un metodo per avere il prezzo base
-- il prodotto esponga un metodo per avere il prezzo comprensivo di iva
-- il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice-nome
-Nello stesso package aggiungete una classe Main con metodo main nella quale testate tutte
- le funzionalità della classe Prodotto.
-BONUS: create un metodo (statico) che restituisca il codice con un pad left
-di 0 per arrivare a 8 caratteri
-(ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è)
+*V Un prodotto è caratterizzato da: - codice (numero intero) - nome - descrizione - prezzo - iva
+*V Usate opportunamente i livelli di accesso (public, private), i costruttori,
+*Vi metodi getter e setter ed eventuali altri metodi di “utilità” per fare in modo che:
+*V- alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
+*V- il codice prodotto sia accessibile solo in lettura
+*V- gli altri attributi siano accessibili sia in lettura che in scrittura
+*V- il prodotto esponga un metodo per avere il prezzo base
+*V- il prodotto esponga un metodo per avere il prezzo comprensivo di iva
+*- il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice-nome
+*Nello stesso package aggiungete una classe Main con metodo main nella quale testate tutte
+*le funzionalità della classe Prodotto.
+*BONUS: create un metodo (statico) che restituisca il codice con un pad left
+*di 0 per arrivare a 8 caratteri
+*(ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è)
  */
 package org.lessons.java.shop;
 
@@ -77,7 +77,13 @@ public class Prodotto {
 		this.iva = iva;
 	}
 	
+	public double getPrezzoBase() {
+	        return prezzo;
+	}
 	
+	public double getPrezzoIva() {
+        return prezzo + (prezzo * iva / 100);
+    }
 	
 	
 }
