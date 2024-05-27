2b13a0924066c5ba479f005c75baac67fc295fb2
Dariya Kashtan <kashtandariya@gmail.com>
2024-05-27 18:15:02 +0200

    attributi e costruttore iniziali

---
+++ src/org/lessons/java/shop/Prodotto.java
@@ -0,0 +1,34 @@
+/*V creare la classe Prodotto che gestisce i prodotti dello shop.
+ *V Un prodotto è caratterizzato da: - codice (numero intero) - nome - descrizione - prezzo - iva
+Usate opportunamente i livelli di accesso (public, private), i costruttori,
+i metodi getter e setter ed eventuali altri metodi di “utilità” per fare in modo che:
+- alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
+- il codice prodotto sia accessibile solo in lettura
+- gli altri attributi siano accessibili sia in lettura che in scrittura
+- il prodotto esponga un metodo per avere il prezzo base
+- il prodotto esponga un metodo per avere il prezzo comprensivo di iva
+- il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice-nome
+Nello stesso package aggiungete una classe Main con metodo main nella quale testate tutte
+ le funzionalità della classe Prodotto.
+BONUS: create un metodo (statico) che restituisca il codice con un pad left
+di 0 per arrivare a 8 caratteri
+(ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è)
+ */
+package org.lessons.java.shop;
+
+public class Prodotto {
+	private final int codice;
+	private String nome;
+	private String descrizione;
+	private double prezzo;
+	private double iva;
+	
+	public Prodotto(int codice, String nome, String descrizione, double prezzo, double iva) {
+		this.codice = codice;
+		this.nome = nome;
+		this.descrizione = descrizione;
+		this.prezzo = prezzo;
+		this.iva = iva;
+	}
+	
+}
